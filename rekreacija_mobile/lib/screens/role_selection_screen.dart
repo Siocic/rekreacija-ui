91c1e5bdd8ee1a515b002b22a75c13337ae85b67
Siocic <faris.siocic@gmail.com>
2024-11-13 18:25:15 +0100

    fix role selection screen

--- rekreacija_mobile/lib/screens/role_selection_screen.dart
+++ rekreacija_mobile/lib/screens/role_selection_screen.dart
@@ -11,62 +11,70 @@ class RoleSelectionScreen extends StatelessWidget {
       backgroundColor: Color.fromRGBO(14, 121, 115, 100),
       body: Center(
         child: Container(
-          width: 400,
-          height: 400,
-          child: Column(
-            children: [
-              Text(
-                'Register here',
-                style: GoogleFonts.sora(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white),
-              ),
-              const SizedBox(height: 50),
-              SizedBox(
-                width: 400,
-                height: 50,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>FizickoliceScreen()));
-                  },
-                  style: TextButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(255, 255, 255, 100),
-                      foregroundColor: Colors.black,
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero)),
-                  child: Text(
-                    'Fizicko lice',
-                    style: GoogleFonts.sora(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 250),
+                Text(
+                  'Register here',
+                  style: GoogleFonts.sora(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                ),
+                const SizedBox(height: 50),
+                SizedBox(
+                  width: 400,
+                  height: 50,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FizickoliceScreen()));
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(255, 255, 255, 100),
+                        foregroundColor: Colors.black,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero)),
+                    child: Text(
+                      'Fizicko lice',
+                      style: GoogleFonts.sora(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: 400,
-                height: 50,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=> PravnoliceScreen()));
-                  },
-                  style: TextButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(255, 255, 255, 100),
-                      foregroundColor: Colors.black,
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero)),
-                  child: Text(
-                    'Pravno lice',
-                    style: GoogleFonts.sora(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: 400,
+                  height: 50,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PravnoliceScreen()));
+                    },
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color.fromRGBO(255, 255, 255, 100),
+                        foregroundColor: Colors.black,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero)),
+                    child: Text(
+                      'Pravno lice',
+                      style: GoogleFonts.sora(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),

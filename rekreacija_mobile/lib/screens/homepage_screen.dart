044e75df430fabc34f00f3018fff597642802f49
Siocic <faris.siocic@gmail.com>
2024-12-11 19:47:04 +0100

    update home screen on mobile

--- rekreacija_mobile/lib/screens/homepage_screen.dart
+++ rekreacija_mobile/lib/screens/homepage_screen.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rekreacija_mobile/routes.dart';
+import 'package:rekreacija_mobile/widgets/hall_card.dart';
 
 class HomePageScreen extends StatefulWidget {
-  HomePageScreen({super.key});
+  const HomePageScreen({super.key});
 
   @override
   State<StatefulWidget> createState() => _HomePageScreen();
 }
 
 class _HomePageScreen extends State<HomePageScreen> {
+  final List<Map<String, String>> objectNearYou = [
+    {
+      'name': 'Univerzitetska dvorana',
+      'address': 'Univerzitetska 1',
+    },
+    {
+      'name': 'Tusanj',
+      'address': 'Rudarska bb',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+  ];
+  final List<Map<String, String>> mostPopularHalls = [
+    {
+      'name': 'Katolicki skolski centar',
+      'address': 'Klosterska',
+    },
+    {
+      'name': 'Gimnazija Ismet Mujezinovic',
+      'address': 'Ulica bb',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -64,6 +93,141 @@ class _HomePageScreen extends State<HomePageScreen> {
                   ),
                 ),
               ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 15.0),
+                    Row(
+                      children: [
+                        const Padding(padding: EdgeInsets.all(5.0)),
+                        Text(
+                          'Popular Objects',
+                          style: GoogleFonts.suezOne(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        const Icon(
+                          Icons.local_fire_department_outlined,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 250.0,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: mostPopularHalls.length,
+                        itemBuilder: (context, index) {
+                          final popularHalls = mostPopularHalls[index];
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                    context, '/hallDetails');
+                              },
+                              child: HallCard(
+                                  hallAdress: popularHalls['name'] ?? '',
+                                  hallName: popularHalls['name'] ?? ''),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 15.0),
+                    Row(
+                      children: [
+                        const Padding(padding: EdgeInsets.all(5.0)),
+                        Text(
+                          'Halls Near You',
+                          style: GoogleFonts.suezOne(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        const Icon(
+                          Icons.location_on,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 250.0,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: objectNearYou.length,
+                        itemBuilder: (context, index) {
+                          final nearYou = objectNearYou[index];
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: InkWell(
+                               onTap: () {
+                                Navigator.pushNamed(
+                                    context, '/hallDetails');
+                              },
+                              child: HallCard(
+                                  hallAdress: nearYou['name'] ?? '',
+                                  hallName: nearYou['name'] ?? ''),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 15.0),
+                    Row(
+                      children: [
+                        const Padding(padding: EdgeInsets.all(5.0)),
+                        Text(
+                          'Favorite objects',
+                          style: GoogleFonts.suezOne(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                        const SizedBox(width: 10.0),
+                        const Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 250.0,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: objectNearYou.length,
+                        itemBuilder: (context, index) {
+                          final nearYou = objectNearYou[index];
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                    context, '/hallDetails');
+                              },
+                              child: HallCard(
+                                  hallAdress: nearYou['name'] ?? '',
+                                  hallName: nearYou['name'] ?? ''),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
---
+++ rekreacija_mobile/lib/widgets/hall_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class HallCard extends StatefulWidget {
+  final String hallName;
+  final String hallAdress;
+
+  const HallCard({super.key, required this.hallAdress, required this.hallName});
+
+  @override
+  State<StatefulWidget> createState() => _HallCardState();
+}
+
+class _HallCardState extends State<HallCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color.fromRGBO(49, 49, 49, 0.8),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SizedBox(
+        width: 250.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Container(
+                width: 250.0,
+                height: 120.0,
+                color: Colors.grey[300],
+                child: const Icon(Icons.image, size: 50, color: Colors.grey),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 0.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            widget.hallName,
+                            style: const TextStyle(
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            widget.hallAdress,
+                            style: const TextStyle(
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.star, color: Colors.yellow, size: 21.0),
+                  SizedBox(width: 5.0),
+                  Text('5.0', style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+          ],
+          
+        ),
+
+      ),
+    );
+  }
+}

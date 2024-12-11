f0ec497fa6a5c171351f46b72c3c29ea52f20c2b
Siocic <faris.siocic@gmail.com>
2024-12-11 19:45:14 +0100

    update objekti screen on mobile

--- rekreacija_mobile/lib/screens/objekti_screen.dart
+++ rekreacija_mobile/lib/screens/objekti_screen.dart
@@ -1,72 +1,250 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:rekreacija_mobile/widgets/bottom_navigation.dart';
-// import 'package:rekreacija_mobile/widgets/custom_appbar.dart';
+import 'package:rekreacija_mobile/widgets/sport_section.dart';
 
 class ObjektiScreen extends StatefulWidget {
-  ObjektiScreen({super.key});
+  const ObjektiScreen({super.key});
 
   @override
   State<StatefulWidget> createState() => _ObjektiScreenState();
 }
 
 class _ObjektiScreenState extends State<ObjektiScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  final List<String> sports = [
+    "Football",
+    "Basketball",
+    "Handball",
+    "Volleyball",
+    "Tennis"
+  ];
+  String selectedSport = "Football";
+
+  final List<Map<String, String>> halls = [
+    {
+      'name': 'Univerzitetska dvorana',
+      'address': 'Univerzitetska 1',
+    },
+    {
+      'name': 'Tusanj',
+      'address': 'Rudarska bb',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Mejdan',
+      'address': 'Bosne Srebrene',
+    },
+    {
+      'name': 'Univerzitetska dvorana',
+      'address': 'Univerzitetska 1',
+    },
+    {
+      'name': 'Tusanj',
+      'address': 'Rudarska bb',
+    },
+    {
+      'name': 'Univerzitetska dvorana',
+      'address': 'Univerzitetska 1',
+    },
+    {
+      'name': 'Tusanj',
+      'address': 'Rudarska bb',
+    },
+  ];
+
+  List<Map<String, String>> _filteredHalls = [];
+  @override
+  void initState() {
+    super.initState();
+    _filteredHalls = halls; 
+  }
+
+  void _filterHalls(String query) {
+    setState(() {
+      if (query.isEmpty) {
+        _filteredHalls = halls; 
+      } else {
+        _filteredHalls = halls
+            .where((hall) =>
+                hall['name']!.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return
-        // Scaffold(
-        //   // appBar:AppBar(
-        //   //   backgroundColor:Color.fromARGB(200, 19, 19, 19),
-        //   //   foregroundColor: Colors.white,
-        //   //   title: const Text('Objekti'),
-        //   // ),
-        //   appBar: CustomAppBar(title: 'Objekti',titleAlignment: Alignment.centerLeft,showBackButton: true,),
-        //   //bottomNavigationBar: CustomBottomNavigation(),
-        //    body:
-        Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(225, 19, 19, 19),
-            Color.fromARGB(225, 49, 49, 49),
-          ],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(225, 19, 19, 19),
+              Color.fromARGB(225, 49, 49, 49),
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
         ),
-      ),
-      child: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 50.0),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
+            const SizedBox(height: 60.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                children: [
+                  Text(
                     'Objekti',
                     style: GoogleFonts.suezOne(
                         color: Colors.white,
                         fontWeight: FontWeight.w400,
                         fontSize: 20.0),
                   ),
-                ),
-                const Spacer(),
-                const Padding(
-                  padding: EdgeInsets.only(right: 10.0),
-                  child: Icon(
+                  const Spacer(),
+                  const Icon(
                     Icons.map_sharp,
                     color: Colors.white,
                   ),
-                )
-              ],
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _searchController,
+                    onChanged:
+                        _filterHalls,
+                    decoration: InputDecoration(
+                      fillColor: const Color.fromRGBO(49, 49, 49, 0.8),
+                      filled: true,
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      hintText: 'Search for a hall...',
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 10.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    style: const TextStyle(color: Colors.white), 
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: sports.map((sport) {
+                  final isSelected = sport == selectedSport;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedSport = sport;
+                        });
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: isSelected
+                            ? const Color.fromRGBO(14, 119, 62, 1.0)
+                            : Colors.white,
+                        foregroundColor:
+                            isSelected ? Colors.white : Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                          side: const BorderSide(color: Colors.white),
+                        ),
+                      ),
+                      child: Text(
+                        sport,
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/hallDetails');
+                        },
+                        child: ListView.builder(
+                          itemCount: _filteredHalls.length,
+                          itemBuilder: (context, index) {
+                            final hall = _filteredHalls[index];
+                            return SportSection(
+                                hallAdress: hall['name'] ?? '',
+                                hallName: hall['address'] ?? '',
+                                onFavoritePressed: () {});
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
-
-    //);
   }
 }
---
+++ rekreacija_mobile/lib/widgets/sport_section.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class SportSection extends StatefulWidget {
+  final String hallName;
+  final String hallAdress;
+  final VoidCallback? onFavoritePressed;
+
+  const SportSection(
+      {super.key,
+      required this.hallAdress,
+      required this.hallName,
+      required this.onFavoritePressed});
+
+  @override
+  State<StatefulWidget> createState() => _SportSectionState();
+}
+
+class _SportSectionState extends State<SportSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color.fromRGBO(49, 49, 49, 0.8),
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SizedBox(
+        height: 120.0,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Container(
+                width: 100.0,
+                height: 120.0,
+                color: Colors.grey[300],
+                child: const Icon(Icons.image, size: 50, color: Colors.grey),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.hallName,
+                      style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                      widget.hallAdress,
+                      style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.favorite_border,
+                  color: Colors.red,
+                ),
+                onPressed: widget.onFavoritePressed,
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}

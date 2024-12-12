6ea6da01fba23281de90c486454f64fed004fb32
Siocic <faris.siocic@gmail.com>
2024-12-13 00:06:17 +0100

    add colors file on desktop app

---
+++ rekreacija_desktop/lib/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Colors for Notification and Review Card
+  static const Color cardColor = Colors.white;
+  static const Color textCardColor = Color.fromRGBO(70, 66, 85, 100);
+  static const Color dateCardColor = Color.fromRGBO(163, 163, 163, 100);
+
+  //Button Colors
+  static const Color buttonGreen = Color.fromRGBO(18, 140, 126, 100);
+  static const Color buttonRed = Colors.red;
+
+  //Text Colors
+  static const Color textWhite = Colors.white;
+  static const Color textBlack = Colors.black;
+  static const Color textBlue = Colors.blue;
+  static const Color textGreen = Colors.green;
+
+  //Error Color
+  static const Color errorColor = Colors.red;
+
+  //Icon Colors
+  static const Color iconYellow = Colors.yellow;
+  static const Color iconGreen = Colors.green;
+  static const Color iconBlack = Colors.black;
+
+  //Underline Color
+  static const Color underlineBlue = Colors.blue;
+}

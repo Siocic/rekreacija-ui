7fdd33743725e77503cc498f4b6e806f29d044b3
Ishak Isabegovic <ishak.isabegovic@edu.fit.ba>
2025-06-14 13:38:54 +0200

    update holiday_model

--- rekreacija_desktop/lib/models/holiday_model.g.dart
+++ rekreacija_desktop/lib/models/holiday_model.g.dart
@@ -9,14 +9,14 @@ part of 'holiday_model.dart';
 HolidayModel _$HolidayModelFromJson(Map<String, dynamic> json) => HolidayModel(
       id: (json['id'] as num?)?.toInt(),
       name: json['name'] as String,
-      startDate: DateTime.parse(json['startDate'] as String),
-      endDate: DateTime.parse(json['endDate'] as String),
+      startDate: DateTime.parse(json['start_date'] as String),
+      endDate: DateTime.parse(json['end_date'] as String),
     );
 
 Map<String, dynamic> _$HolidayModelToJson(HolidayModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
-      'startDate': instance.startDate.toIso8601String(),
-      'endDate': instance.endDate.toIso8601String(),
+      'start_date': instance.startDate.toIso8601String(),
+      'end_date': instance.endDate.toIso8601String(),
     };

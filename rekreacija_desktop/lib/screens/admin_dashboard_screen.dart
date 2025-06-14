eaa6ebc01223af596ef0aee6d188d8fb57cd4f07
Ishak Isabegovic <ishak.isabegovic@edu.fit.ba>
2025-06-14 14:16:13 +0200

    add chart for reservation trends on admin dashboard

--- rekreacija_desktop/lib/screens/admin_dashboard_screen.dart
+++ rekreacija_desktop/lib/screens/admin_dashboard_screen.dart
@@ -2,8 +2,10 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fl_chart/fl_chart.dart';
+import 'package:rekreacija_desktop/models/appointment_model.dart';
 import 'package:rekreacija_desktop/models/object_count_per_user_model.dart';
 import 'package:rekreacija_desktop/models/review_model.dart';
+import 'package:rekreacija_desktop/providers/appointment_provider.dart';
 import 'package:rekreacija_desktop/providers/auth_provider.dart';
 import 'package:rekreacija_desktop/providers/object_provider.dart';
 import 'package:rekreacija_desktop/providers/review_provider.dart';
@@ -23,12 +25,14 @@ class _AdminDashboard extends State<AdminDashboard> {
   late ObjectProvider objectProvider;
   late AuthProvider authProvider;
   late ReviewProvider reviewProvider;
+  late AppointmentProvider appointmentProvider;
   int numberOfObjects = 0;
   int numberOfUsers = 0;
   bool isObjects = true;
   bool isUser = true;
   List<ReviewModel> reviewList = [];
   List<ObjectCountPerUserModel> objectsPerUser = [];
+  List<AppointmentModel> allAppointment = [];
 
   @override
   void initState() {
@@ -36,11 +40,14 @@ class _AdminDashboard extends State<AdminDashboard> {
     objectProvider = context.read<ObjectProvider>();
     authProvider = context.read<AuthProvider>();
     reviewProvider = context.read<ReviewProvider>();
+    appointmentProvider = context.read<AppointmentProvider>();
     fetchData();
   }
 
   Map<int, int> ratingCounts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
   Map<String, int> countObjectPerUser = {};
+  Map<String, int> totalCounts = {};
+  Map<String, int> approvedCounts = {};
 
   Future<void> fetchData() async {
     try {
@@ -48,6 +55,7 @@ class _AdminDashboard extends State<AdminDashboard> {
       var users = await authProvider.getAllUser();
       var review = await reviewProvider.Get();
       var countedObject = await objectProvider.getCountObjectPerUser();
+      var appoinmentList = await appointmentProvider.Get();
 
       setState(() {
         numberOfObjects = objects.length;
@@ -56,6 +64,7 @@ class _AdminDashboard extends State<AdminDashboard> {
         objectsPerUser = countedObject;
         isObjects = false;
         isUser = false;
+        allAppointment = appoinmentList;
 
         for (var r in reviewList) {
           int rating = r.rating?.round() ?? 0;
@@ -68,6 +77,16 @@ class _AdminDashboard extends State<AdminDashboard> {
           int count = c.objectCount ?? 0;
           countObjectPerUser[userName.toString()] =
               (countObjectPerUser[userName] ?? 0) + count;
+        }
+        for (var appt in allAppointment) {
+          final date = DateTime.parse(appt.appointment_date.toString());
+          final key = "${date.year}-${date.month.toString().padLeft(2, '0')}";
+
+          totalCounts[key] = (totalCounts[key] ?? 0) + 1;
+
+          if (appt.is_approved == true) {
+            approvedCounts[key] = (approvedCounts[key] ?? 0) + 1;
+          }
         }
       });
     } catch (e) {
@@ -145,138 +164,254 @@ class _AdminDashboard extends State<AdminDashboard> {
     });
 /*PIE CHART FOR OBJECTS OF USERS --END*/
 
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      const Padding(
-        padding: EdgeInsets.all(40.0),
-        child: ContentHeader(title: 'Dashboard'),
-      ),
-      const SizedBox(height: 30),
-      Padding(
-        padding: const EdgeInsets.only(left: 40),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CardView(
-                icon: Icons.apartment,
-                num: numberOfObjects,
-                decription: "Total objects",
-                isLoading: isObjects),
-            const SizedBox(width: 10),
-            CardView(
-                icon: Icons.people_alt,
-                num: numberOfUsers,
-                decription: "Total users",
-                isLoading: isUser),
-          ],
-        ),
-      ),
-      const SizedBox(height: 30),
-      Padding(
-        padding: const EdgeInsets.only(left: 40, top: 30, bottom: 10),
-        child: Row(
-          children: [
-            Column(
+/* LINE CHART ZA UKUPNE APPOINTMNETE --START */
+    final sortedKeys = totalCounts.keys
+        .toSet()
+        .union(approvedCounts.keys.toSet())
+        .toList()
+      ..sort((a, b) => a.compareTo(b));
+
+    List<FlSpot> allSpots = [];
+    List<FlSpot> approvedSpots = [];
+
+    for (int i = 0; i < sortedKeys.length; i++) {
+      final key = sortedKeys[i];
+      allSpots.add(FlSpot(i.toDouble(), (totalCounts[key] ?? 0).toDouble()));
+      approvedSpots
+          .add(FlSpot(i.toDouble(), (approvedCounts[key] ?? 0).toDouble()));
+    }
+
+/* LINE CHART ZA UKUPNE APPOINTMNETE --END */
+
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(40.0),
+            child: ContentHeader(title: 'Dashboard'),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.only(left: 40),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CardView(
+                    icon: Icons.apartment,
+                    num: numberOfObjects,
+                    decription: "Total objects",
+                    isLoading: isObjects),
+                const SizedBox(width: 10),
+                CardView(
+                    icon: Icons.people_alt,
+                    num: numberOfUsers,
+                    decription: "Total users",
+                    isLoading: isUser),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, top: 30, bottom: 10),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Reviews by rating",
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      height: 300,
+                      width: 300,
+                      child: BarChart(
+                        BarChartData(
+                          alignment: BarChartAlignment.spaceAround,
+                          maxY: ratingCounts.values
+                                  .reduce((a, b) => a > b ? a : b)
+                                  .toDouble() +
+                              2,
+                          //barTouchData: const BarTouchData(enabled: true),
+                          barTouchData: BarTouchData(
+                            touchTooltipData: BarTouchTooltipData(
+                              getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                                return BarTooltipItem(
+                                  rod.toY.toInt().toString(),
+                                  const TextStyle(color: Colors.white),
+                                );
+                              },
+                            ),
+                          ),
+                          titlesData: FlTitlesData(
+                            leftTitles: const AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            bottomTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: (value, meta) =>
+                                    Text('${value.toInt()}★'),
+                              ),
+                            ),
+                          ),
+                          borderData: FlBorderData(show: false),
+                          barGroups: ratingBars,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 80),
+                Column(
+                  children: [
+                    const Text(
+                      "Object by user",
+                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 80),
+                    SizedBox(
+                      height: 100,
+                      width: 200,
+                      child: PieChart(
+                        PieChartData(
+                          sections: sections,
+                          centerSpaceRadius: 40,
+                          sectionsSpace: 2,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 50),
+                Padding(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: countObjectPerUser.keys
+                        .toList()
+                        .asMap()
+                        .entries
+                        .map((entry) {
+                      //int index = entry.key;
+                      final username = entry.value.toString();
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 12,
+                              height: 12,
+                              color: userColors[username],
+                            ),
+                            const SizedBox(width: 8),
+                            Text(username),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, top: 20, right: 20),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
-                  "Reviews by rating",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  "Reservation Trends",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                const SizedBox(height: 30),
+                const SizedBox(height: 10),
                 SizedBox(
-                  height: 300,
-                  width: 300,
-                  child: BarChart(
-                    BarChartData(
-                      alignment: BarChartAlignment.spaceAround,
-                      maxY: ratingCounts.values
-                              .reduce((a, b) => a > b ? a : b)
-                              .toDouble() +
-                          2,
-                      //barTouchData: const BarTouchData(enabled: true),
-                      barTouchData: BarTouchData(
-                        touchTooltipData: BarTouchTooltipData(
-                          getTooltipItem: (group, groupIndex, rod, rodIndex) {
-                            return BarTooltipItem(
-                              rod.toY.toInt().toString(),
-                              const TextStyle(color: Colors.white),
-                            );
-                          },
-                        ),
-                      ),
+                  height: 250,
+                  width: double.infinity,
+                  child: LineChart(
+                    LineChartData(
                       titlesData: FlTitlesData(
-                        leftTitles: const AxisTitles(
-                          sideTitles: SideTitles(showTitles: false),
-                        ),
                         bottomTitles: AxisTitles(
                           sideTitles: SideTitles(
                             showTitles: true,
-                            getTitlesWidget: (value, meta) =>
-                                Text('${value.toInt()}★'),
+                            getTitlesWidget: (value, _) {
+                              int index = value.toInt();
+                              if (index < sortedKeys.length) {
+                                return Text(
+                                  sortedKeys[index].substring(5), // Month part
+                                  style: const TextStyle(fontSize: 12),
+                                );
+                              }
+                              return const Text('');
+                            },
                           ),
                         ),
-                      ),
-                      borderData: FlBorderData(show: false),
-                      barGroups: ratingBars,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 80),
-            Column(
-              children: [
-                const Text(
-                  "Object by user",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 80),
-                SizedBox(
-                  height: 100,
-                  width: 200,
-                  child: PieChart(
-                    PieChartData(
-                      sections: sections,
-                      centerSpaceRadius: 40,
-                      sectionsSpace: 2,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 50),
-            Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: countObjectPerUser.keys
-                    .toList()
-                    .asMap()
-                    .entries
-                    .map((entry) {
-                  //int index = entry.key;
-                  final username = entry.value.toString();
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4),
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 12,
-                          height: 12,
-                          color: userColors[username],
+                        leftTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: (value, _) => Text(
+                                value.toInt().toString(),
+                                style: const TextStyle(fontSize: 12)),
+                          ),
                         ),
-                        const SizedBox(width: 8),
-                        Text(username),
+                        topTitles:
+                            AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                        rightTitles:
+                            AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      ),
+                      gridData: FlGridData(show: true),
+                      borderData: FlBorderData(show: true),
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: allSpots, // All reservations
+                          isCurved: true,
+                          barWidth: 3,
+                          color: Colors.blue,
+                          dotData: FlDotData(show: true),
+                        ),
+                        LineChartBarData(
+                          spots: approvedSpots, // Approved reservations
+                          isCurved: true,
+                          barWidth: 3,
+                          color: Colors.red,
+                          dotData: FlDotData(show: true),
+                        ),
                       ],
                     ),
-                  );
-                }).toList(),
-              ),
-            )
-          ],
-        ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                // Legend
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        Container(width: 12, height: 12, color: Colors.blue),
+                        const SizedBox(width: 6),
+                        const Text("All Reservations"),
+                      ],
+                    ),
+                    const SizedBox(width: 20),
+                    Row(
+                      children: [
+                        Container(width: 12, height: 12, color: Colors.red),
+                        const SizedBox(width: 6),
+                        const Text("Approved Reservations"),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-    ]);
+    );
   }
 }

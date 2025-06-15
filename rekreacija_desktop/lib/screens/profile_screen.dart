e281f0803cb33be5db53a71d13bbfa7e23d96334
Ishak Isabegovic <ishak.isabegovic@edu.fit.ba>
2025-06-15 09:42:00 +0200

    fix profile screen on desktop app remove singlechildscrollview

--- rekreacija_desktop/lib/screens/profile_screen.dart
+++ rekreacija_desktop/lib/screens/profile_screen.dart
@@ -77,91 +77,89 @@ class _ProfileScreenState extends State<ProfileScreen> {
         }
       });
     }
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(40.0),
-            child: ContentHeader(title: 'Profile'),
-          ),
-          Center(
-            child: Container(
-                width: 900.0,
-                height: 700.0,
-                padding: const EdgeInsets.all(15.0),
-                color: Colors.grey[200],
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 80,
-                      backgroundImage: image != null
-                          ? image!.image
-                          : const AssetImage(
-                              'assets/images/RekreacijaDefaultProfilePicture.png'),
-                      backgroundColor: Colors.grey,
-                    ),
-                    const SizedBox(height: 20),
-                    ProfileParts(
-                        labelText: 'FirstName',
-                        inputText: controllers["firstName"]!),
-                    const SizedBox(height: 15),
-                    ProfileParts(
-                        labelText: 'LastName',
-                        inputText: controllers["lastName"]!),
-                    const SizedBox(height: 15),
-                    ProfileParts(
-                        labelText: 'Email', inputText: controllers["email"]!),
-                    const SizedBox(height: 15),
-                    ProfileParts(
-                        labelText: 'City', inputText: controllers["city"]!),
-                    const SizedBox(height: 15),
-                    ProfileParts(
-                        labelText: 'Address', inputText: controllers["address"]!),
-                    const SizedBox(height: 15),
-                    ProfileParts(
-                        labelText: 'Phone', inputText: controllers["phone"]!),
-                    const SizedBox(height: 30),
-                    SizedBox(
-                      width: 500.0,
-                      height: 50,
-                      child: TextButton(
-                          onPressed: () async {
-                            final bool? result = await showDialog<bool>(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return EditProfile(
-                                    firstNameController:
-                                        controllers["firstName"]!,
-                                    lastNameController: controllers["lastName"]!,
-                                    emailController: controllers["email"]!,
-                                    cityController: controllers["city"]!,
-                                    addressController: controllers["address"]!,
-                                    phoneController: controllers["phone"]!,
-                                    currentImage: image?.image ??
-                                        const AssetImage(
-                                            'assets/images/RekreacijaDefaultProfilePicture.png'),
-                                  );
-                                });
-                            if (result == true) {
-                              _loadUserProfile();
-                            }
-                          },
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.grey,
-                              foregroundColor: Colors.black,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16.0))),
-                          child: const Text(
-                            'Edit profile',
-                            style: TextStyle(fontSize: 18),
-                          )),
-                    )
-                  ],
-                )),
-          )
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(40.0),
+          child: ContentHeader(title: 'Profile'),
+        ),
+        Center(
+          child: Container(
+              width: 900.0,
+              height: 700.0,
+              padding: const EdgeInsets.all(15.0),
+              color: Colors.grey[200],
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: image != null
+                        ? image!.image
+                        : const AssetImage(
+                            'assets/images/RekreacijaDefaultProfilePicture.png'),
+                    backgroundColor: Colors.grey,
+                  ),
+                  const SizedBox(height: 20),
+                  ProfileParts(
+                      labelText: 'FirstName',
+                      inputText: controllers["firstName"]!),
+                  const SizedBox(height: 15),
+                  ProfileParts(
+                      labelText: 'LastName',
+                      inputText: controllers["lastName"]!),
+                  const SizedBox(height: 15),
+                  ProfileParts(
+                      labelText: 'Email', inputText: controllers["email"]!),
+                  const SizedBox(height: 15),
+                  ProfileParts(
+                      labelText: 'City', inputText: controllers["city"]!),
+                  const SizedBox(height: 15),
+                  ProfileParts(
+                      labelText: 'Address', inputText: controllers["address"]!),
+                  const SizedBox(height: 15),
+                  ProfileParts(
+                      labelText: 'Phone', inputText: controllers["phone"]!),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: 500.0,
+                    height: 50,
+                    child: TextButton(
+                        onPressed: () async {
+                          final bool? result = await showDialog<bool>(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return EditProfile(
+                                  firstNameController:
+                                      controllers["firstName"]!,
+                                  lastNameController: controllers["lastName"]!,
+                                  emailController: controllers["email"]!,
+                                  cityController: controllers["city"]!,
+                                  addressController: controllers["address"]!,
+                                  phoneController: controllers["phone"]!,
+                                  currentImage: image?.image ??
+                                      const AssetImage(
+                                          'assets/images/RekreacijaDefaultProfilePicture.png'),
+                                );
+                              });
+                          if (result == true) {
+                            _loadUserProfile();
+                          }
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.grey,
+                            foregroundColor: Colors.black,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0))),
+                        child: const Text(
+                          'Edit profile',
+                          style: TextStyle(fontSize: 18),
+                        )),
+                  )
+                ],
+              )),
+        )
+      ],
     );
   }
 }
